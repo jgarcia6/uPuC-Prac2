f6903765232bd0240f12d439781df554919c2df5
jgarcia6 <garcia.jesus@uabc.edu.mx>
2020-09-29 12:21:13 -0700

    Seed files
    Started code and seed files

---
+++ bulls_and_cows.c
@@ -0,0 +1,23 @@
+// To be defined in ASM as public procedures
+extern void putchar(char data);
+extern char getchar(void);
+
+// To be defined in C
+void gets(char *str);
+void puts(char *str);
+void itoa(unsigned int number, char* str, unsigned char base);
+unsigned int atoi(char *str);
+
+typedef struct bullsAndCowsTag{
+    unsigned bulls:4;
+    unsigned cows:4;
+} bullsAndCowsType;
+
+bullsAndCowsType evalBullsAndCows(char *secrete, char *guess);
+
+int main(void)
+{
+    // Port Prac1 to achieve same behavior
+    return 0;
+}
+
